d003059a688c533a445860d6d75ee55699b1c16b
Markus Heilig <markusheilig@gmx.net>
2017-04-04 19:11:47 +0200

    websocket + twitter

--- webserver/index.js
+++ webserver/index.js
@@ -1,28 +1,81 @@
-'use strict';
 
-const Hapi = require('hapi');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var _ = require('lodash');
+var twitterConfig = require('./twitterConfig');
+var Twitter = require('node-tweet-stream')
+  , t = new Twitter(twitterConfig);
 
-// Create a server with a host and port
-const server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello', 
-    handler: function (request, reply) {
-        return reply('hello world');
-    }
+t.on('tweet', (tweet) => {
+    Object.keys(subscriptions).forEach((keyword) => {        
+        if (tweet.text.includes(keyword)) {            
+            subscriptions[keyword].forEach((socket) => {
+                socket.emit('tweet', {
+                    keyword: keyword,
+                    text: tweet.text                    
+                });
+            });
+        }
+    });    
 });
 
-// Start the server
-server.start((err) => {
+t.on('error', (err) => {
+    console.log('twitter-stream-error', err)
+});
 
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+const subscriptions = {};
+
+const subscribe = function(socket, keyword) {
+    if (!subscriptions[keyword]) {        
+        subscriptions[keyword] = [socket];
+        t.track(keyword);        
+    } else if (subscriptions[keyword].filter((s) => s === socket).length === 0) {        
+        subscriptions[keyword].push(socket);
+    }    
+};
+
+const unsubscribe = function(socket, keyword) {    
+    if (subscriptions[keyword]) {
+        subscriptions[keyword] = subscriptions[keyword].filter((s) => s !== socket);
+        if (subscriptions[keyword].length === 0) {
+            t.untrack(keyword);            
+            subscriptions[keyword] = [];
+        }
+    }     
+};
+
+const removeAllSubscriptions = function(socket) {
+    Object.keys(subscriptions).forEach((keyword) => {
+        unsubscribe(keyword, socket);        
+    });
+};
+
+io.on('connection', function (socket) {
+
+    console.log('connection', socket.id);    
+
+    socket.on('subscribe', function (keyword) {        
+        console.log('subscribe', keyword);
+        subscribe(socket, keyword);        
+    });
+
+    socket.on('unsubscribe', function(keyword) {
+        console.log('unsubscribe', keyword);
+        unsubscribe(socket, keyword);        
+    });
+
+    socket.on('disconnect', function () {
+        console.log('disconnect', socket.id);
+        removeAllSubscriptions(socket);        
+    });
+
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
