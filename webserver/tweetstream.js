c40ea687b8b0c8cd253ca1c798355923e5e3a207
Markus Heilig <markusheilig@gmx.net>
2017-04-22 13:58:01 +0200

    adjust indent

--- webserver/tweetstream.js
+++ webserver/tweetstream.js
@@ -11,83 +11,83 @@ const keywords = ['trump'];
 var receivedNewKeywords = false;
 
 const installErrorHandler = function (stream) {
-    stream.on('error', (err) => {
-        console.log('ERROR', err);
-    });
+  stream.on('error', (err) => {
+    console.log('ERROR', err);
+  });
 };
 
 const installResponseHandler = function (stream) {
-    stream.on('response', (res) => {
-        var tweet = '';
-        res.on('data', (chunk) => {
-            // we received a new chunk from twitter
-            const str = chunk.toString();
-            // check if check contains a new tweet (tweets are separated by '\r\n')
-            if (str.includes('\r\n')) {                
-                const splitted = str.split('\r\n');
-                // first part belongs to 'current' tweet
-                tweet += splitted[0];
-                try {
-                    // publish tweet
-                    const tweetAsJson = JSON.parse(tweet);
-                    channel.sendToQueue(q, Buffer.from(tweetAsJson.text));
-                } catch (ignored) { /* should never happen */ }
-                // now the new tweet message
-                tweet = splitted[1];
-            } else {
-                // the whole chunk belongs to the current tweet
-                tweet += str;
-            }
-        });
+  stream.on('response', (res) => {
+    var tweet = '';
+    res.on('data', (chunk) => {
+      // we received a new chunk from twitter
+      const str = chunk.toString();
+      // check if check contains a new tweet (tweets are separated by '\r\n')
+      if (str.includes('\r\n')) {                
+        const splitted = str.split('\r\n');
+        // first part belongs to 'current' tweet
+        tweet += splitted[0];
+        try {
+          // publish tweet
+          const tweetAsJson = JSON.parse(tweet);
+          channel.sendToQueue(q, Buffer.from(tweetAsJson.text));
+        } catch (ignored) { /* should never happen */ }
+        // now the new tweet message
+        tweet = splitted[1];
+      } else {
+        // the whole chunk belongs to the current tweet
+        tweet += str;
+      }
     });
+  });
 };
 
 const createTwitterStream = function () {
-    var stream = request.post({
-        url: 'https://stream.twitter.com/1.1/statuses/filter.json?filter_level=none&stall_warnings=true',
-        oauth: {
-            consumer_key: process.env.TWITTER_CONSUMER_KEY,
-            token: process.env.TWITTER_TOKEN,
-            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-            token_secret: process.env.TWITTER_TOKEN_SECRET
-        },
-        form: {
-            track: keywords.join(',')
-        }
-    });
-    return stream;
+  var stream = request.post({
+    url: 'https://stream.twitter.com/1.1/statuses/filter.json?filter_level=none&stall_warnings=true',
+    oauth: {
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      token: process.env.TWITTER_TOKEN,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      token_secret: process.env.TWITTER_TOKEN_SECRET
+    },
+    form: {
+      track: keywords.join(',')
+    }
+  });
+  return stream;
 };
 
 const connectToTwitter = function() {
-    stream = createTwitterStream();
-    installErrorHandler(stream);
-    installResponseHandler(stream);
+  stream = createTwitterStream();
+  installErrorHandler(stream);
+  installResponseHandler(stream);
 };
 
 amqp.connect(process.env.RABBITMQ_URL)
     .then((conn) => {
-        return conn.createChannel();
+      return conn.createChannel();
     })
     .then((ch) => {
-        channel = ch;
-        channel.assertQueue(q, { durable: false });
-        connectToTwitter();        
-        reconnect();
+      channel = ch;
+      channel.assertQueue(q, { durable: false });
+      connectToTwitter();        
+      reconnect();
     })
     .catch((err) => {
-        console.log(err);
+      console.log(err);
     });
 
 const reconnect = function() {
-    // when we received new keywords, we have to create a new connection to twitter
-    // but we dont want to do this to often because we are afraid of being blocked!
-    const timeoutInSeconds = 30;
-    setInterval(() => {        
-        if (receivedNewKeywords) {
-            console.log('reconnect to twitter...');
-            stream.abort();
-            connectToTwitter();
-        }
-    }, timeoutInSeconds * 1000);
+  // when we received new keywords, we have to create a new connection to twitter
+  // but we dont want to do this to often because we are afraid of being blocked!
+  const timeoutInSeconds = 30;
+  setInterval(() => {        
+    if (receivedNewKeywords) {
+        console.log('reconnect to twitter...');
+        stream.abort();
+        connectToTwitter();
+    }
+  }, timeoutInSeconds * 1000);
 };
 
