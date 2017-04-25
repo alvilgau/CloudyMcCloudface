dbe65df4c9e6458b99f8c89e7f461c07b2c79b8a
Timo Weiß <info@timo-weiss.com>
2017-04-25 20:44:41 +0200

    rename local err variables to not confuse with outer scope vars

--- webserver/tweetlogger.js
+++ webserver/tweetlogger.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const amqp = require('amqplib/callback_api');
 
 amqp.connect(process.env.RABBITMQ_URL, (err, conn) => {
-  conn.createChannel((err, ch) => {
+  conn.createChannel((createChannelErr, ch) => {
     const ex = 'analyzed_tweets';
     ch.assertExchange(ex, 'fanout', { durable: false });
-    ch.assertQueue('', { exclusive: true }, (err, q) => {
+    ch.assertQueue('', { exclusive: true }, (assertQueueErr, q) => {
       console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', q.queue);
       ch.bindQueue(q.queue, ex, '');
       ch.consume(q.queue, (msg) => {
