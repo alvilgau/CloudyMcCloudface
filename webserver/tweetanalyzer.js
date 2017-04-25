430c7c39dbbcf273491cb5e94d8ae776d0c11b90
Markus Heilig <markusheilig@gmx.net>
2017-04-25 21:35:22 +0200

    set exchange durable false

--- webserver/tweetanalyzer.js
+++ webserver/tweetanalyzer.js
@@ -151,7 +151,7 @@ amqp.connect(process.env.RABBITMQ_URL, (err, conn) => {
 
   // assert exchange for keyword observation
   conn.createChannel((err, ch) => {
-    ch.assertExchange('keywords', 'fanout', {durable: true});
+    ch.assertExchange('keywords', 'fanout', {durable: false});
     ch.assertQueue('', {exclusive: true}, function(err, q) {
       ch.bindQueue(q.queue, 'keywords', '');
       ch.consume(q.queue, function(msg) {
