8a902e07001c42872d30da93073aeab7eedcbefc
Andreas Reinhardt <andreasreinhardt7@gmail.com>
2017-05-05 14:41:35 +0200

    Add basic plotting with time on x-axis

---
+++ web/graph.elm
@@ -0,0 +1,63 @@
+module Graph exposing (viewGraph)
+
+import Plot exposing (..)
+import Svg.Attributes exposing (stroke)
+import Date
+import Time
+
+
+viewGraph keywords data =
+    let
+        plotCustomizations_ =
+            { defaultSeriesPlotCustomizations | horizontalAxis = timeAxis }
+    in
+        viewSeriesCustom
+            plotCustomizations_
+            (List.map (\k -> lineColoredWhereKeyword k.keyword k.color) keywords)
+            data
+
+
+lineColoredWhereKeyword keyword colors =
+    { axis = sometimesYouDoNotHaveAnAxis
+    , interpolation = Monotone Nothing [ stroke colors ]
+    , toDataPoints = List.filter (\dp -> dp.keyword == keyword) >> List.map (\dp -> clear dp.time dp.y)
+    }
+
+
+timeAxis : Axis
+timeAxis =
+    customAxis <|
+        \summary ->
+            { position = closestToZero
+            , axisLine = Just (simpleLine summary)
+            , ticks = List.map simpleTick (decentPositions summary)
+            , labels = List.map timeLabel (decentPositions summary)
+            , flipAnchor = False
+            }
+
+
+timeLabel : Time.Time -> LabelCustomizations
+timeLabel position =
+    { position = position
+    , view = viewLabel [] (Date.fromTime position |> dateToTimeString)
+    }
+
+
+dateToTimeString : Date.Date -> String
+dateToTimeString date =
+    let
+        hour =
+            Date.hour date
+                |> toString
+
+        minute =
+            Date.minute date
+                |> toString
+                |> String.padLeft 2 '0'
+
+        second =
+            Date.second date
+                |> toString
+                |> String.padLeft 2 '0'
+    in
+        hour ++ ":" ++ minute ++ ":" ++ second
--- web/main.elm
+++ web/main.elm
@@ -1,4 +1,15 @@
-import Html exposing (text)
+module Main exposing (..)
+
+import Graph exposing (..)
+
 
 main =
-    text "Hello World!"
+    viewGraph
+        [ { keyword = "trump", color = "red" }, { keyword = "clinton", color = "blue" } ]
+        [ { keyword = "trump", time = 1493985180, y = 1 }
+        , { keyword = "trump", time = 1494000180, y = 10 }
+        , { keyword = "trump", time = 1494015180, y = 3 }
+        , { keyword = "clinton", time = 1493985180, y = 1 }
+        , { keyword = "clinton", time = 1494000180, y = 3 }
+        , { keyword = "clinton", time = 1494015180, y = 7 }
+        ]
