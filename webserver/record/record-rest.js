a5d7773048abbc25e1141d44e4b724a6ae2d7951
Alex Vilgauk <alex@vilgauk.de>
2017-06-10 16:43:11 +0200

    record tweets

--- webserver/record/record-rest.js
+++ webserver/record/record-rest.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const Hapi = require('hapi');
 const dynamo = require('./dynamo-api');
+const redisCommands = require('../redis/redis-commands');
+const redisEvents = require('../redis/redis-events');
 
 const server = new Hapi.Server();
 server.connection({
@@ -8,12 +10,31 @@ server.connection({
     port: 3010
 });
 
+const defaultTenant = {
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    token: process.env.TWITTER_TOKEN,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    tokenSecret: process.env.TWITTER_TOKEN_SECRET,
+};
+
 server.route({
     method: 'GET',
     path: '/tweets/{tenantId}/{keyword}',
     handler: function (request, reply) {
         return reply(dynamo.queryTweets(request.params.tenantId, request.params.keyword,
             request.query.begin, request.query.end));
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/record/{tenantId}/{keyword}',
+    handler: function (request, reply) {
+        redisCommands.trackKeywords(defaultTenant, 'system', 'trump');
+        redisEvents.subscribe(redisCommands.getId(defaultTenant), 'system', (tenantId, userId, analyzedTweets) => {
+            console.log(analyzedTweets);
+        });
+        return reply("ok");
     }
 });
 
