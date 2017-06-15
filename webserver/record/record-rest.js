89db0a7aa426e12c1e844293331d6a772d49f1bd
Markus Heilig <markusheilig@gmx.net>
2017-06-15 11:13:35 +0200

    set indentation level to 2

--- webserver/record/record-rest.js
+++ webserver/record/record-rest.js
@@ -8,7 +8,7 @@ const server = new Hapi.Server();
 server.connection({
   host: 'localhost',
   port: process.env.RECORD_REST_PORT,
-  routes: { cors: true }
+  routes: {cors: true}
 });
 
 const THREE_SECONDS = 3000;
@@ -24,71 +24,74 @@ const ONE_MINUTE = 60000;
    }
  */
 server.route({
-    method: 'POST',
-    path: '/records',
-    handler: function (request, reply) {
-        const payload = request.payload;
-        const currentTime = new Date().getTime();
+  method: 'POST',
+  path: '/records',
+  handler: function (request, reply) {
+    const payload = request.payload;
+    const currentTime = new Date().getTime();
 
-        if (!payload.begin && !payload.end) {
-            // no time set, then record now for 1 minute
-            payload.begin = currentTime + THREE_SECONDS;
-            payload.end = payload.begin + ONE_MINUTE;
-        }
-        else if (!(payload.begin && payload.end)) {
-            return reply(Boom.badData({type: 'error', message: 'When 1 time parameter is set, the other time parameter must be set too'}));
-        }
-        else if (payload.begin < currentTime) {
-            return reply(Boom.badData({type: 'error', message: 'begin must be in the future.'}));
-        }
-        else if (payload.end < payload.begin) {
-            return reply(Boom.badData({type: 'error', message: 'end must be after begin.'}));
-        }
-
-        dynamoRecords.insertRecord(payload).then(record => {
-            redisCommands.scheduleRecording(record.id, record.begin, record.end);
-            return reply(record);
-        });
-
+    if (!payload.begin && !payload.end) {
+      // no time set, then record now for 1 minute
+      payload.begin = currentTime + THREE_SECONDS;
+      payload.end = payload.begin + ONE_MINUTE;
     }
+    else if (!(payload.begin && payload.end)) {
+      return reply(Boom.badData({
+        type: 'error',
+        message: 'When 1 time parameter is set, the other time parameter must be set too'
+      }));
+    }
+    else if (payload.begin < currentTime) {
+      return reply(Boom.badData({type: 'error', message: 'begin must be in the future.'}));
+    }
+    else if (payload.end < payload.begin) {
+      return reply(Boom.badData({type: 'error', message: 'end must be after begin.'}));
+    }
+
+    dynamoRecords.insertRecord(payload).then(record => {
+      redisCommands.scheduleRecording(record.id, record.begin, record.end);
+      return reply(record);
+    });
+
+  }
 });
 
 /**
  * Route to get all records for a specific tenantId
  */
 server.route({
-    method: 'GET',
-    path: '/tenants/{tenantId}/records',
-    handler: function (request, reply) {
-        return reply(dynamoRecords.scanRecordsByTenant(request.params.tenantId));
-    }
+  method: 'GET',
+  path: '/tenants/{tenantId}/records',
+  handler: function (request, reply) {
+    return reply(dynamoRecords.scanRecordsByTenant(request.params.tenantId));
+  }
 });
 
 /**
  * Route to get the analyzed tweets for one record
  */
 server.route({
-    method: 'GET',
-    path: '/records/{recordId}/tweets',
-    handler: function (request, reply) {
-        dynamoRecords.getRecord(request.params.recordId)
-            .then(record => {
-                if (!record) {
-                    return Boom.notFound('Record not found.');
-                }
-                const tenantId = redisCommands.getId(record.tenant);
-                return dynamoTweets.queryTweets(tenantId, record.id);
-            })
-            .then(reply);
-    }
+  method: 'GET',
+  path: '/records/{recordId}/tweets',
+  handler: function (request, reply) {
+    dynamoRecords.getRecord(request.params.recordId)
+      .then(record => {
+        if (!record) {
+          return Boom.notFound('Record not found.');
+        }
+        const tenantId = redisCommands.getId(record.tenant);
+        return dynamoTweets.queryTweets(tenantId, record.id);
+      })
+      .then(reply);
+  }
 });
 
 server.start((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
 
-    // creating records table if not exist
-    dynamoRecords.createTable();
+  // creating records table if not exist
+  dynamoRecords.createTable();
 });
