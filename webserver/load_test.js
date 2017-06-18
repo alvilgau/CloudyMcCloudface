da94f3bae9c8d719cc44bb2df094493637504ac3
Alex Vilgauk <alex@vilgauk.de>
2017-06-18 23:15:37 +0200

    load test improved

--- webserver/load_test.js
+++ webserver/load_test.js
@@ -1,38 +1,53 @@
+const sleep = require('thread-sleep');
+
 const WebSocket = require('ws');
 
-const numOfSockets = process.argv[2] || 500;
+const numOfSockets = process.argv[2] || 100;
 const sockets = [];
+let scount = 0;
 const url = '';
 
 const keywords = ['trump', 'obama', 'twitter', 'london', 'apple', 'beer', 'dog', 'cat', 'life', 'party', 'sunday', 'monday', 'work', 'train', 'sf', 'drink', 'hangover'];
 
 const getFiveRandomKeywords = () => {
-  const kw = [];
-  for (let i = 0; i < 5; i++) {
-    const item = keywords[Math.floor(Math.random()*keywords.length)];
-    kw.push(item);
-  }
-  return kw;
+    const kw = [];
+    for (let i = 0; i < 5; i++) {
+        const item = keywords[Math.floor(Math.random() * keywords.length)];
+        kw.push(item);
+    }
+    return kw;
 };
 
 const createWebSocketConnection = () => {
 
-  const ws = new WebSocket(url);
-  const keywordsToTrack = getFiveRandomKeywords();
-  ws.on('open', () => {
-    console.log('opened connection');
-    ws.send(JSON.stringify({
-      tenant: null,
-      keywords: keywordsToTrack
-    }));
-  });
-  ws.on('error', console.error);
+    const ws = new WebSocket(url);
+    const keywordsToTrack = getFiveRandomKeywords();
+    ws.on('open', () => {
+        sleep(200);
+        scount++;
+        console.log('opened connection', scount);
+        if(scount === numOfSockets) {
+            // setTimeout(() => {
+            //     sockets.forEach(socket => {
+            //         socket.close()
+            //     });
+            // }, 120000);
+        }
+        setTimeout(() => {
+            ws.send(JSON.stringify({
+                tenant: null,
+                keywords: keywordsToTrack
+            }));
+        }, 1000);
 
-  ws.on('message', (data) => {
-    console.log(data);
-  });
+    });
+    ws.on('error', console.error);
 
-  sockets.push(ws);
+    ws.on('message', (data) => {
+        console.log(data);
+    });
+
+    sockets.push(ws);
 };
 
 // create one connection
@@ -40,13 +55,8 @@ createWebSocketConnection();
 
 // wait 2 seconds, then open a lot of other connections
 setTimeout(() => {
-  for (let i = 0; i < numOfSockets - 1; i++) {
-    createWebSocketConnection();
-  }
-}, 2000);
+    for (let i = 0; i < numOfSockets - 1; i++) {
+        createWebSocketConnection();
+    }
+}, 3000);
 
-setTimeout(() => {
-  sockets.forEach(socket => {
-    socket.close()
-  });
-}, 120000);
