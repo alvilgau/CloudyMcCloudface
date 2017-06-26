4c96f05eb013e225dab0bae873ea0a184935dc7f
Alex Vilgauk <alex@vilgauk.de>
2017-06-26 11:42:11 +0200

    deploy

--- codedeploy/install.sh
+++ codedeploy/install.sh
@@ -12,18 +12,18 @@ cd /home/ec2-user/CloudyMcCloudface/webserver
 forever stopall
 if [ "$DEPLOYMENT_GROUP_NAME" == "cloudy_webserver" ]
 then
-    forever start -o out.log -e err.log webserver.js
+    forever start -a -o /var/log/cloudy-out.log -e /var/log/cloudy-err.log webserver.js
     forever start log/dynamodb-logger.js webserver INFO out.log
     forever start log/dynamodb-logger.js webserver ERROR err.log
     forever start log/log-rest.js
 elif [ "$DEPLOYMENT_GROUP_NAME" == "cloudy_stream" ]
 then
-    forever start -o out.log -e err.log stream/main-tweetstream.js
+    forever start -a -o /var/log/cloudy-out.log -e /var/log/cloudy-err.log stream/main-tweetstream.js
     forever start log/dynamodb-logger.js tweetstream INFO out.log
     forever start log/dynamodb-logger.js tweetstream ERROR err.log
 elif [ "$DEPLOYMENT_GROUP_NAME" == "cloudy_recorder" ]
 then
-    forever start -o out.log -e err.log record/recorder.js
+    forever start -a -o /var/log/cloudy-out.log -e /var/log/cloudy-err.log record/recorder.js
     forever start log/dynamodb-logger.js recorder INFO out.log
     forever start log/dynamodb-logger.js recorder ERROR err.log
 fi
